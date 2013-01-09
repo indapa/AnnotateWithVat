2b7a5178cbad2b99a3ddb8b03c8ce503960a70bd
indapa <indapa@gmail.com>
2013-01-08 22:13:28 -0500

    initial add getGenocodeAnnotations.sh

---
+++ getGencodeAnnotations.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ ! -d GencodeAnnotations ]
+then
+mkdir GencodeAnnotations
+fi
+cd GencodeAnnotations
+echo "Retrieving Coding Sequence (CDS) annotation sets"
+wget -nc \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode3b.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode3b.fa \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode3c.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode3c.fa \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode4.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode4.fa \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode5.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode5.fa \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode6.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode6.fa \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode7.interval \
+     http://homes.gersteinlab.org/people/lh372/VAT/gencode7.fa
