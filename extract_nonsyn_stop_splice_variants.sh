b13bc72560a2ad73a09a301af8400a04c7d58014
indapa <indapa@gmail.com>
2013-01-14 11:36:44 -0500

    initial commit extract_nonsyn_stop_splice_variants.sh

---
+++ extract_nonsyn_stop_splice_variants.sh
@@ -0,0 +1,8 @@
+if test -z "$1"
+then
+echo "provide a bgzip-ed vcf file as first argument."
+echo "extract_nonsyn_stop_splice_variants.sh file.vcf.gz"
+exit
+fi
+zcat $1 | egrep '(prematureStop|removedStop|spliceOverlap|nonsynonymous)'
+#cat  ALL.2of4intersection.20100804.sites.chr2:1-50000000.vcf | snpMapper vat-2.0.1/Gencode/gencode7.interval vat-2.0.1/Gencode/gencode7.fa 
