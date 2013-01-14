b8429ae44f329cdd85fc4cc82c5d409f82dc7a04
indapa <indapa@gmail.com>
2013-01-14 14:01:48 -0500

    print header lines with tabix prior to egrep-ing annotations out

--- extract_fs_splice_indels.sh
+++ extract_fs_splice_indels.sh
@@ -4,5 +4,15 @@ echo "provide a bgzip-ed vcf file as first argument."
 echo "extract_nonsyn_stop_splice_variants.sh file.vcf.gz"
 exit
 fi
+
+if [ ! -e $1.tbi ]
+then
+echo "tabix index (*.tbi) doesn't exist"
+exit
+fi
+tabix -H $1
+
+
+
 zcat $1 | egrep '(insertionFS|deletionFS|spliceOverlap)'
 #cat  ALL.2of4intersection.20100804.sites.chr2:1-50000000.vcf | snpMapper vat-2.0.1/Gencode/gencode7.interval vat-2.0.1/Gencode/gencode7.fa 
